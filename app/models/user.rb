a60ca3fb61dfe0b45041d75f61fb0fd324b6659b
木村　幸介 <kosukekimura0528@gmail.com>
2019-06-06 10:59:54 +0900

    add admin, description column user model / add validates on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,11 @@ class User < ActiveRecord::Base
         :validatable
   include DeviseTokenAuth::Concerns::User
 
+  validates :name, presence: true, length: { maximum: 64 }
+  validates :email, presence: true, uniqueness: true, format:{with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :admin, presence: true, default: false
+  validates :description, length:{ maximum: 1000 }
+
   has_many :assigns, dependent: :destroy
   has_many :groups, dependent: :destroy, foreign_key: :owner_id
 end
---
+++ db/migrate/20190606013247_add_admin_and_description_column_to_user.rb
@@ -0,0 +1,6 @@
+class AddAdminAndDescriptionColumnToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :admin, :boolean, null: false, default: false
+    add_column :users, :description, :text
+  end
+end
